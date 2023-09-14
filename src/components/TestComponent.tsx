add26278cba127b4bd73f1cca8d5e2bfbef76c0c
Artem Nikitin <a.nikitin@dataduck.com>
2023-09-14 13:55:29 +0200

    add button

--- src/components/TestComponent.tsx
+++ src/components/TestComponent.tsx
@@ -1,12 +1,17 @@
 import MediaQuery from "react-responsive";
 
 export const TestComponent = (): JSX.Element => {
+	const clickHandler = (): void => {
+		console.log("press");
+		console.log(window);
+	};
 	return (
 		<>
 			<MediaQuery minWidth={960}>
 				<div className="">Screen Big</div>
 			</MediaQuery>
 			<div className="">Screen Smol</div>
+			<button onClick={clickHandler}>BUTTON</button>
 		</>
 	);
 };
